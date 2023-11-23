33b4d76258c41699a92f0066554dee69b1ac65c1
wendel portela <carlosribeirodesouzac@gmail.com>
2023-11-23 09:07:23 -0300

    Merge branch 'main' of https://github.com/thetheu122/pizzaria-corleone

--- backend/banco de dados/insert.sql
+++ backend/banco de dados/insert.sql
@@ -28,7 +28,7 @@ INSERT INTO tb_tipo_produto ( ds_tipo_produto )
                      
 INSERT INTO tb_tipo_produto ( ds_tipo_produto ) 
 					 VALUES ( 'salgado');
-                  -- aaaaaaaaaa   
+                  -- aaaaaa   
 
 INSERT INTO tb_tipo_produto ( ds_tipo_produto ) 
 					 VALUES ( 'vegano');       
